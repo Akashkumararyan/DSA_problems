ce86ba06e2e57c149a6865dbbf77172d2c6286dc
Akashkumararyan <92356691+Akashkumararyan@users.noreply.github.com>
2023-02-01 22:40:09 +0530

    Create sorting.cpp

---
+++ sorting.cpp
@@ -0,0 +1,334 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!\n";
+}
+
+Input: nums = [4,1,4,0,3,5]
+Output: 2
+Explanation:
+1. Remove 0 and 5, and the average is (0 + 5) / 2 = 2.5. Now, nums = [4,1,4,3].
+2. Remove 1 and 4. The average is (1 + 4) / 2 = 2.5, and nums = [4,3].
+3. Remove 3 and 4, and the average is (3 + 4) / 2 = 3.5.
+Since there are 2 distinct numbers among 2.5, 2.5, and 3.5, we return 2.
+Example 2:
+
+Input: nums = [1,100]
+Output: 1
+Explanation:
+There is only one average to be calculated after removing 1 and 100, so we return 1.
+
+int distinctAverages(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int i=0;
+        int j=n-1;
+        set<double> s;
+        while(i<j){
+            double avg=(nums[i++] + nums[j--])/2.0;
+            s.insert(avg);
+        }
+        return s.size();
+    }
+
+
+Input: n = 12, k = 3
+Output: 3
+Explanation: Factors list is [1, 2, 3, 4, 6, 12], the 3rd factor is 3.
+Example 2:
+
+Input: n = 7, k = 2
+Output: 7
+Explanation: Factors list is [1, 7], the 2nd factor is 7.
+Example 3:
+
+Input: n = 4, k = 4
+Output: -1
+Explanation: Factors list is [1, 2, 4], there is only 3 factors. We should return -1.
+
+
+  int kthFactor(int n, int k) {
+       vector<int> v;
+       int ans=-1;
+       for(int i=1;i<=n;i++){
+           if(n%i==0)
+           v.push_back(i);
+       } 
+       for(int i=0;i<v.size();i++){
+           if(i==k-1)
+           ans=v[i];
+       }
+       return ans;
+    }
+
+int kthFactor(int n, int k) {
+       vector<int> v;
+       int ans=-1;
+       for(int i=1;i<=n;i++){
+           if(n%i==0)
+           v.push_back(i);
+       } 
+       if(v.size()<k) return -1;
+         return v[k-1];
+       }
+
+
+
+Example 1:
+
+Input: words = ["abba","baba","bbaa","cd","cd"]
+Output: ["abba","cd"]
+Explanation:
+One of the ways we can obtain the resultant array is by using the following operations:
+- Since words[2] = "bbaa" and words[1] = "baba" are anagrams, we choose index 2 and delete words[2].
+  Now words = ["abba","baba","cd","cd"].
+- Since words[1] = "baba" and words[0] = "abba" are anagrams, we choose index 1 and delete words[1].
+  Now words = ["abba","cd","cd"].
+- Since words[2] = "cd" and words[1] = "cd" are anagrams, we choose index 2 and delete words[2].
+  Now words = ["abba","cd"].
+We can no longer perform any operations, so ["abba","cd"] is the final answer.
+Example 2:
+
+Input: words = ["a","b","c","d","e"]
+Output: ["a","b","c","d","e"]
+Explanation:
+No two adjacent strings in words are anagrams of each other, so no operations are performed.
+
+
+vector<string> removeAnagrams(vector<string>& words) {
+        for(int i = 1;i<words.size();i++){
+            string x = words[i];
+            sort(x.begin(),x.end());
+            string y = words[i-1];
+            sort(y.begin(),y.end());
+            if(x == y){
+                words.erase(words.begin() + i);
+                i--;
+            }
+        }
+        return words;
+    }
+
+
+
+Example 1:
+
+Input: arr = [3,5,1]
+Output: true
+Explanation: We can reorder the elements as [1,3,5] or [5,3,1] with differences 2 and -2 respectively, between each consecutive elements.
+Example 2:
+
+Input: arr = [1,2,4]
+Output: false
+Explanation: There is no way to reorder the elements to obtain an arithmetic progression.
+
+
+
+  bool canMakeArithmeticProgression(vector<int>& arr) {
+        set<int> s;
+        sort(arr.begin(),arr.end());
+        for(int i=1;i<arr.size();i++){
+            s.insert(arr[i]-arr[i-1]);
+        }
+        if(s.size()==1) return true;
+
+        return false;
+    }
+
+
+
+Example 1:
+
+Input: cost = [1,2,3]
+Output: 5
+Explanation: We buy the candies with costs 2 and 3, and take the candy with cost 1 for free.
+The total cost of buying all candies is 2 + 3 = 5. This is the only way we can buy the candies.
+Note that we cannot buy candies with costs 1 and 3, and then take the candy with cost 2 for free.
+The cost of the free candy has to be less than or equal to the minimum cost of the purchased candies.
+Example 2:
+
+Input: cost = [6,5,7,9,2,2]
+Output: 23
+Explanation: The way in which we can get the minimum cost is described below:
+- Buy candies with costs 9 and 7
+- Take the candy with cost 6 for free
+- We buy candies with costs 5 and 2
+- Take the last remaining candy with cost 2 for free
+Hence, the minimum cost to buy all candies is 9 + 7 + 5 + 2 = 23.
+Example 3:
+
+Input: cost = [5,5]
+Output: 10
+Explanation: Since there are only 2 candies, we buy both of them. There is not a third candy we can take for free.
+Hence, the minimum cost to buy all candies is 5 + 5 = 10.
+
+
+
+
+int minimumCost(vector<int>& cost) {
+        int ans=0;
+        int cnt=0;
+        sort(cost.begin(),cost.end(),greater());
+        for(int i=0;i<cost.size();i++){
+        if(cnt==2){
+            cnt=0;
+            continue;
+        }
+        ans += cost[i];
+        cnt++;
+          
+        }
+        return ans;
+    }
+
+
+
+Example 1:
+
+Input: seats = [3,1,5], students = [2,7,4]
+Output: 4
+Explanation: The students are moved as follows:
+- The first student is moved from from position 2 to position 1 using 1 move.
+- The second student is moved from from position 7 to position 5 using 2 moves.
+- The third student is moved from from position 4 to position 3 using 1 move.
+In total, 1 + 2 + 1 = 4 moves were used.
+Example 2:
+
+Input: seats = [4,1,5,9], students = [1,3,2,6]
+Output: 7
+Explanation: The students are moved as follows:
+- The first student is not moved.
+- The second student is moved from from position 3 to position 4 using 1 move.
+- The third student is moved from from position 2 to position 5 using 3 moves.
+- The fourth student is moved from from position 6 to position 9 using 3 moves.
+In total, 0 + 1 + 3 + 3 = 7 moves were used.
+
+
+
+
+  int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        int ans=0;
+        sort(seats.begin(),seats.end());
+        sort(students.begin(),students.end());
+        for(int i=0;i<seats.size();i++){
+            ans+=abs(seats[i]-students[i]);
+        }
+        
+        return ans;
+    }
+
+
+
+
+Example 1:
+
+Input: piles = [2,4,1,2,7,8]
+Output: 9
+Explanation: Choose the triplet (2, 7, 8), Alice Pick the pile with 8 coins, you the pile with 7 coins and Bob the last one.
+Choose the triplet (1, 2, 4), Alice Pick the pile with 4 coins, you the pile with 2 coins and Bob the last one.
+The maximum number of coins which you can have are: 7 + 2 = 9.
+On the other hand if we choose this arrangement (1, 2, 8), (2, 4, 7) you only get 2 + 4 = 6 coins which is not optimal.
+Example 2:
+
+Input: piles = [2,4,5]
+Output: 4
+Example 3:
+
+Input: piles = [9,8,7,6,5,1,2,3,4]
+Output: 18
+
+
+
+int maxCoins(vector<int>& piles) {
+        sort(piles.begin(),piles.end());
+        int sum=0,i=0,j=piles.size()-2;
+        for(;i<j;j=j-2){
+            i++;
+            sum += piles[j];
+        }
+        return sum;
+    }
+
+
+
+Example 1:
+
+Input: nums = [3,6,1,2,5], k = 2
+Output: 2
+Explanation:
+We can partition nums into the two subsequences [3,1,2] and [6,5].
+The difference between the maximum and minimum value in the first subsequence is 3 - 1 = 2.
+The difference between the maximum and minimum value in the second subsequence is 6 - 5 = 1.
+Since two subsequences were created, we return 2. It can be shown that 2 is the minimum number of subsequences needed.
+Example 2:
+
+Input: nums = [1,2,3], k = 1
+Output: 2
+Explanation:
+We can partition nums into the two subsequences [1,2] and [3].
+The difference between the maximum and minimum value in the first subsequence is 2 - 1 = 1.
+The difference between the maximum and minimum value in the second subsequence is 3 - 3 = 0.
+Since two subsequences were created, we return 2. Note that another optimal solution is to partition nums into the two subsequences [1] and [2,3].
+Example 3:
+
+Input: nums = [2,2,4,5], k = 0
+Output: 3
+Explanation:
+We can partition nums into the three subsequences [2,2], [4], and [5].
+The difference between the maximum and minimum value in the first subsequences is 2 - 2 = 0.
+The difference between the maximum and minimum value in the second subsequences is 4 - 4 = 0.
+The difference between the maximum and minimum value in the third subsequences is 5 - 5 = 0.
+Since three subsequences were created, we return 3. It can be shown that 3 is the minimum number of subsequences needed.
+
+
+   int partitionArray(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end(),greater<int>());
+        int l=0,ans=0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[l]-nums[i]>k){
+                ans++;
+                l=i;
+            }
+        }
+        return ans+1;
+    }
+
+
+
+Example 1:
+
+Input: arr = [3,3,3,3,5,5,5,2,2,7]
+Output: 2
+Explanation: Choosing {3,7} will make the new array [5,5,5,2,2] which has size 5 (i.e equal to half of the size of the old array).
+Possible sets of size 2 are {3,5},{3,2},{5,2}.
+Choosing set {2,7} is not possible as it will make the new array [3,3,3,3,5,5,5] which has a size greater than half of the size of the old array.
+Example 2:
+
+Input: arr = [7,7,7,7,7,7]
+Output: 1
+Explanation: The only possible set you can choose is {7}. This will make the new array empty.
+
+
+  int minSetSize(vector<int>& arr) {
+        int n=arr.size();
+        int cnt=1;
+        vector<int> v;
+        sort(arr.begin(),arr.end());
+        for(int i=1;i<n;i++){
+            if(arr[i]==arr[i-1]){
+                cnt++;
+            }
+            else{
+                v.push_back(cnt);
+                cnt=1;
+            }
+        }
+        v.push_back(cnt);
+        sort(v.begin(),v.end());
+        int a=n/2,ans=0;
+        for(int i=v.size()-1;a>0 && i>=0;i--){
+            ans++;
+            a-=v[i];
+        }
+        return ans;
+    }
