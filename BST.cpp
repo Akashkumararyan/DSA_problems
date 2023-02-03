2ee5a2105a217aad2c782215dbd70ef16156300b
Akashkumararyan <92356691+Akashkumararyan@users.noreply.github.com>
2023-02-03 18:51:19 +0530

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -436,5 +436,14 @@ public:
         return catalan_number(n);
     }
 
+//recursion method
 
+ int numTrees(int n) {
+        if(n<=1) return 1;
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans += numTrees(i-1)*numTrees(n-i);
+        }
+        return ans;
+    }
 
