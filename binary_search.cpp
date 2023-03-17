088d64128c52fca999e7a6c62f6ac923378428e9
Akashkumararyan <92356691+Akashkumararyan@users.noreply.github.com>
2023-03-17 16:25:13 +0530

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -154,3 +154,71 @@ nt specialArray(vector<int>& nums) {
 		}
 		return -1;
 	}
+
+1011. Capacity To Ship Packages Within D Days
+
+Input: weights = [1,2,3,4,5,6,7,8,9,10], days = 5
+Output: 15
+Explanation: A ship capacity of 15 is the minimum to ship all the packages in 5 days like this:
+1st day: 1, 2, 3, 4, 5
+2nd day: 6, 7
+3rd day: 8
+4th day: 9
+5th day: 10
+
+Note that the cargo must be shipped in the order given, so using a ship of capacity 14 and splitting the packages into parts like (2, 3, 4, 5), (1, 6, 7), (8), (9), (10) is not allowed.
+Example 2:
+
+Input: weights = [3,2,2,4,1,4], days = 3
+Output: 6
+Explanation: A ship capacity of 6 is the minimum to ship all the packages in 3 days like this:
+1st day: 3, 2
+2nd day: 2, 4
+3rd day: 1, 4
+Example 3:
+
+Input: weights = [1,2,3,1,1], days = 4
+Output: 3
+Explanation:
+1st day: 1
+2nd day: 2
+3rd day: 3
+4th day: 1, 1
+	
+	 bool canShip(vector<int>& weights,int days,int maxCapacity){
+          int currentDay=1,currentCapacity=0;
+          for(int weight:weights){
+              if(weight > maxCapacity) return false;
+              if(weight+currentCapacity > maxCapacity){
+                  currentDay += 1;
+                  if(currentDay > days) return false;
+                  currentCapacity=weight;
+              }
+              else{
+                  currentCapacity += weight;
+              }
+          }
+          return true;
+      }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int capacity=0,maxWeight=0,sumWeight=0;
+        for(int weight:weights){
+            maxWeight=max(maxWeight,weight);
+            sumWeight += weight;
+        }
+        int start=maxWeight,end=sumWeight;
+        while(start<=end){
+            int mid=start + (end-start)/2;
+            if(canShip(weights,days,mid)){
+                capacity=mid;
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return capacity;
+    }
+};
+
