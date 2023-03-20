6f90511bb876559684e2ac23858f951d87d8fae0
Akashkumararyan <92356691+Akashkumararyan@users.noreply.github.com>
2023-03-20 22:42:41 +0530

    Update bit_manipulation.cpp

--- bit_manipulation.cpp
+++ bit_manipulation.cpp
@@ -246,3 +246,54 @@ Output: [1,0]
 
         return v;
     }
+    
+
+1829. Maximum XOR for Each Query
+
+Input: nums = [0,1,1,3], maximumBit = 2
+Output: [0,3,2,3]
+Explanation: The queries are answered as follows:
+1st query: nums = [0,1,1,3], k = 0 since 0 XOR 1 XOR 1 XOR 3 XOR 0 = 3.
+2nd query: nums = [0,1,1], k = 3 since 0 XOR 1 XOR 1 XOR 3 = 3.
+3rd query: nums = [0,1], k = 2 since 0 XOR 1 XOR 2 = 3.
+4th query: nums = [0], k = 3 since 0 XOR 3 = 3.
+Example 2:
+
+Input: nums = [2,3,4,7], maximumBit = 3
+Output: [5,2,6,5]
+Explanation: The queries are answered as follows:
+1st query: nums = [2,3,4,7], k = 5 since 2 XOR 3 XOR 4 XOR 7 XOR 5 = 7.
+2nd query: nums = [2,3,4], k = 2 since 2 XOR 3 XOR 4 XOR 2 = 7.
+3rd query: nums = [2,3], k = 6 since 2 XOR 3 XOR 6 = 7.
+4th query: nums = [2], k = 5 since 2 XOR 5 = 7.
+Example 3:
+
+Input: nums = [0,1,2,2,5,7], maximumBit = 3
+Output: [4,3,6,4,6,7]
+
+vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        int n=nums.size();
+        int mx=pow(2,maximumBit)-1;
+        vector<int> ans;
+        int x=0;
+        for(int i=0;i<n;i++){
+           x ^= nums[i];
+           ans.push_back(mx^x);
+        }
+        reverse(ans.begin(),ans.end());
+       return ans;
+    }
+};
+
+2588. Count the Number of Beautiful Subarrays
+
+Example 1:
+
+Input: arr = [2,3,1,6,7]
+Output: 4
+Explanation: The triplets are (0,1,2), (0,2,2), (2,3,4) and (2,4,4)
+Example 2:
+
+Input: arr = [1,1,1,1,1]
+Output: 10
+  
