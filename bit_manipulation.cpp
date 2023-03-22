b49344216a8cbe86bd2cb8e9b409aad1ee8b7a13
Akashkumararyan <92356691+Akashkumararyan@users.noreply.github.com>
2023-03-22 20:10:41 +0530

    Update bit_manipulation.cpp

--- bit_manipulation.cpp
+++ bit_manipulation.cpp
@@ -297,3 +297,36 @@ Example 2:
 Input: arr = [1,1,1,1,1]
 Output: 10
   
+
+  
+  2275. Largest Combination With Bitwise AND Greater Than Zero
+  
+  Input: candidates = [16,17,71,62,12,24,14]
+Output: 4
+Explanation: The combination [16,17,62,24] has a bitwise AND of 16 & 17 & 62 & 24 = 16 > 0.
+The size of the combination is 4.
+It can be shown that no combination with a size greater than 4 has a bitwise AND greater than 0.
+Note that more than one combination may have the largest size.
+For example, the combination [62,12,24,14] has a bitwise AND of 62 & 12 & 24 & 14 = 8 > 0.
+Example 2:
+
+Input: candidates = [8,8]
+Output: 2
+Explanation: The largest combination [8,8] has a bitwise AND of 8 & 8 = 8 > 0.
+The size of the combination is 2, so we return 2.
+  
+  
+  int largestCombination(vector<int>& candidates) {
+        vector<int> v(32,0);
+        int ans = 0,x,j;
+        for(auto &i: candidates){
+            for(j = 0; j <= 31; j++){
+                if(i&(1<<j)){
+                    v[j]++;
+                }
+                ans = max(ans,v[j]);
+            }
+        }
+        return ans;
+    }
+};
