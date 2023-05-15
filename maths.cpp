42d044fddfd65c331d68960a07e5591b0cb2c4ab
Akashkumararyan <92356691+Akashkumararyan@users.noreply.github.com>
2023-05-15 16:16:28 +0530

    Update maths.cpp

--- maths.cpp
+++ maths.cpp
@@ -420,3 +420,34 @@ The arrangement with the smallest value that does not contain any leading zeros 
         return stoll(s);
     }
 };
+
+
+2544. Alternating Digit Sum
+
+Example 1:
+
+Input: n = 521
+Output: 4
+Explanation: (+5) + (-2) + (+1) = 4.
+Example 2:
+
+Input: n = 111
+Output: 1
+Explanation: (+1) + (-1) + (+1) = 1.
+  
+   int alternateDigitSum(int n) {
+       vector<int> v;
+       int ans=0;
+       while(n>0){
+           v.push_back(n%10);
+           n=n/10;
+       } 
+       reverse(v.begin(),v.end());
+       for(int i=0;i<v.size();i++){
+           if(i%2==0) ans += v[i];
+           else{
+               ans -= v[i];
+           }
+       }
+       return ans;
+    }
